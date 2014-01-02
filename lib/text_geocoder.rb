f604ba7a1c246df96c616ac6383b2bf034d4107b
davidrs <david.rs@hotmail.com>
2014-01-02 12:03:37 -0500

    Make IATI xml formatting requirments more flexible. ie. langauge doesn't have to explicitly be english.

--- lib/text_geocoder.rb
+++ lib/text_geocoder.rb
@@ -4,11 +4,11 @@ require 'geocoder'
 
 module TextGeocoder
   extend self
-  
+
 	def path_to_resources
 		File.join(File.dirname(File.expand_path(__FILE__)), '')
 	end
-  
+
 	def geocode(contents, options={})
 
 		response = Nokogiri::XML(contents)
@@ -26,7 +26,7 @@ module TextGeocoder
 		File.open(path_to_resources+"/adm1codes.txt", "r:UTF-8").each_line do |line|
 			#get adm2 name
 			whitelist << line.strip.split("\t")[1].strip.split(" ")
-			
+
 			# add ascii name, unless duplicate
 			unless line.strip.split("\t")[1].strip ==  line.strip.split("\t")[2].strip
 				whitelist << line.strip.split("\t")[2].strip.split(" ")
@@ -36,7 +36,7 @@ module TextGeocoder
 		File.open(path_to_resources+"/adm2codes.txt", "r:UTF-8").each_line do |line|
 			#get city name
 			whitelist << line.strip.split("\t")[1].strip.split(" ")
-			
+
 			# add ascii name, unless duplicate
 			unless line.strip.split("\t")[1].strip ==  line.strip.split("\t")[2].strip
 				whitelist << line.strip.split("\t")[2].strip.split(" ")
@@ -47,27 +47,28 @@ module TextGeocoder
 		results=[]
 		# loop each activity
 		response.xpath("//iati-activity").each do |activity|
-			
+
 			# get iati-identifier
 			iati_id = activity.at("iati-identifier").text
 
-			# store recipient country 
+			# store recipient country
 			rcpt_country = activity.at("recipient-country")
+
 			hash={:uid => iati_id}
 			# eliminate records with multi country data
-			if rcpt_country and rcpt_country.attr("percentage") == "100"
-				
+			if rcpt_country and (!rcpt_country.attr("percentage") or rcpt_country.attr("percentage") == "100")
+
 				# find country name
 				country = IsoCountryCodes.find(rcpt_country.attr("code")).name
 
 				# loop each description in activities
 				Nokogiri::XML(activity.to_s).xpath("//description").each do |desc|
 
-					# find the correct and English description 
-					if desc.attr("type") and desc.attr("xml:lang") == "en"
+					# find the correct and English description
+					if desc.attr("type") and (!desc.attr("xml:lang") or desc.attr("xml:lang") == "en")
 
-						# TODO: adjust code to keep groups of uppercase words as a single entity		
-					
+						# TODO: adjust code to keep groups of uppercase words as a single entity
+
 						query=desc.text.scan(/([[:upper:]][[:lower:]]+\s?)+/)
 						query.each do |x|
 							x[0].strip!
@@ -75,15 +76,15 @@ module TextGeocoder
 						query.uniq!
 						hash[:positions]=[]
 						query.each do |q|
-						if whitelist.include? q[0].strip and !blacklist.include? q[0].strip 
+						if whitelist.include? q[0].strip and !blacklist.include? q[0].strip
 								coords=Geocoder.search("#{q[0].strip} #{country}")
-								sleep 0.20
-								
+								sleep 0.40
+
 								unless coords.nil?
 									coords.each do |c|
 
 										if c.country == country
-										
+
 												# TODO: If a geocode request returns multiple responses
 												#			Throw out any results that do not contain the search term in address
 												#			If 2 responses have the exact same address just merge them.
